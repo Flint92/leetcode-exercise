97eb02822aaaba0535d37cbba063fa2e824d236e
flint <flint.han@madeforgoods.com.cn>
2025-01-14 11:29:44 +0800

    string to integer atoi golang edition

--- go/excercise8/string_to_integer_atoi.go
+++ go/excercise8/string_to_integer_atoi.go
@@ -2,36 +2,38 @@ package excercise8
 
 import (
 	"math"
-	"unicode"
+	"strings"
 )
 
 func stringToIntegerAtoi(s string) int {
-	stack := make([]int, 0, len(s))
-	sign := 1
-
-	for _, ch := range []rune(s) {
-		if unicode.IsSpace(ch) {
-			continue
-		}
-		if ch == '-' {
-			if len(stack) == 0 && sign == 1 {
-				sign = -1
-				continue
-			} else {
-				break
-			}
-		}
-		if unicode.IsLetter(ch) {
-			break
-		}
-		if unicode.IsDigit(ch) {
-			stack = append(stack, int(ch-'0'))
-		}
+	s = strings.TrimSpace(s)
+	if len(s) == 0 {
+		return 0
 	}
 
 	result := 0
-	for _, ii := range stack {
-		if (result > math.MaxInt32/10) || (result == math.MaxInt32/10 && ii > math.MaxInt32%10) {
+	sign := 1
+	index := 0
+
+	if s[0] == '-' {
+		sign = -1
+		index = 1
+	} else if s[0] == '+' {
+		index = 1
+		sign = 1
+	}
+
+	for i, ii := range []rune(s) {
+		if i < index {
+			continue
+		}
+
+		digit := int(ii - '0')
+		if digit < 0 || digit > 9 {
+			break
+		}
+
+		if (result > math.MaxInt32/10) || (result == math.MaxInt32/10 && digit > math.MaxInt32%10) {
 			if sign == 1 {
 				return math.MaxInt32
 			} else {
@@ -39,7 +41,7 @@ func stringToIntegerAtoi(s string) int {
 			}
 		}
 
-		result = result*10 + ii
+		result = result*10 + digit
 	}
 
 	return result * sign
